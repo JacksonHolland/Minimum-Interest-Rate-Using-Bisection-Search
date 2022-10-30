a59e4104f5905c8e784b21cdd58f37f686340104
JacksonHolland <116762360+JacksonHolland@users.noreply.github.com>
2022-10-30 00:27:47 -0400

    Update ps1c.py

--- ps1c.py
+++ ps1c.py
@@ -1,14 +1,14 @@
 ##############################################
-## Get user input for initial_deposit below ##
 ##############################################
 initial_deposit = float(input('Enter the initial deposit: '))
 cost_of_house = float(input('Enter the cost of the house: '))
 portion_of_house = (float(input('Enter the down payment percentage: '))/100)
+number_of_years = float(input('Enter the number of years until down payment: '))
 
 #########################################################################
-## Initializing other variables ##
 #########################################################################
 cost_of_down_payment = cost_of_house * portion_of_house
+months = number_of_years * 12
 
 lower_bound = cost_of_down_payment - 100
 upper_bound = cost_of_down_payment + 100
@@ -17,7 +17,7 @@ upper = 1
 lower = 0
 r = .5
 
-final_savings = (initial_deposit * ((1+r/12)**36))
+final_savings = (initial_deposit * ((1+r/12)**months))
 
 control = True
 steps = 0
@@ -29,7 +29,7 @@ if initial_deposit >= lower_bound:
     print(f'Best savings rate: {r} [or very close to this number]')
     control = False
 
-if initial_deposit * (1+1/12)**36 < lower_bound:
+if initial_deposit * (1+1/12)**months < lower_bound:
     r = None
     print('The initial deposit is not high enough to pay the down payment after three years.')
     control = False
@@ -45,5 +45,5 @@ while control:
         control = False
     r = (upper + lower) / 2
     steps += 1
-    final_savings = (initial_deposit * ((1+r/12)**36))
+    final_savings = (initial_deposit * ((1+r/12)**months))
 
