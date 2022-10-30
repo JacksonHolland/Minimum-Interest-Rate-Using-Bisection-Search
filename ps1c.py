78ed151d77aee81628e728fc3e03a544d903822e
JacksonHolland <116762360+JacksonHolland@users.noreply.github.com>
2022-10-29 23:58:33 -0400

    Add files via upload

---
+++ ps1c.py
@@ -0,0 +1,55 @@
+## 6.100A PSet 1: Part C
+## Name: Jackson Holland
+## Time Spent: 1.25 hours
+## Collaborators: NA
+
+##############################################
+## Get user input for initial_deposit below ##
+##############################################
+initial_deposit = float(input('Enter the initial deposit: '))
+cost_of_house = float(input('Enter the cost of the house: '))
+portion_of_house = (float(input('Enter the down payment percentage: '))/100)
+
+#########################################################################
+## Initialize other variables you need (if any) for your program below ##
+#########################################################################
+cost_of_down_payment = cost_of_house * portion_of_house
+
+lower_bound = cost_of_down_payment - 100
+upper_bound = cost_of_down_payment + 100
+
+upper = 1
+lower = 0
+r = .5
+
+final_savings = (initial_deposit * ((1+r/12)**36))
+
+control = True
+steps = 0
+
+##################################################################################################
+## Determine the lowest rate of return needed to get the down payment for your dream home below ##
+##################################################################################################
+if initial_deposit >= lower_bound:
+    r = 0
+    print(f'Best savings rate: {r} [or very close to this number]')
+    control = False
+
+if initial_deposit * (1+1/12)**36 < lower_bound:
+    r = None
+    print('The initial deposit is not high enough to pay the down payment after three years.')
+    control = False
+
+while control:
+    if final_savings < lower_bound:
+        lower = r
+    elif final_savings > upper_bound:
+        upper = r
+    else:
+        print(f'Best savings rate: {r} [or very close to this number]')
+        print(f'Steps in bisection search: {steps} [or very close to this number]')
+        control = False
+    r = (upper + lower) / 2
+    steps += 1
+    final_savings = (initial_deposit * ((1+r/12)**36))
+
